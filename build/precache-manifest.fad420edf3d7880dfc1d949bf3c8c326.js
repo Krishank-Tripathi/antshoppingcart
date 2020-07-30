7d5cf003da3e817f38ad184bd0e55e51ef408c4c
Krishank-Tripathi <43090677+Krishank-Tripathi@users.noreply.github.com>
2020-07-30 14:05:11 +0530

    add remainig prod files

---
+++ build/precache-manifest.fad420edf3d7880dfc1d949bf3c8c326.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a6265bb26e5949a3b1b01fac458f56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "355b82b810ee5d2616eb",
+    "url": "/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "a74c11634f30fe7510ce",
+    "url": "/static/css/main.20a8cefe.chunk.css"
+  },
+  {
+    "revision": "355b82b810ee5d2616eb",
+    "url": "/static/js/2.4dc9a34b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4dc9a34b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a74c11634f30fe7510ce",
+    "url": "/static/js/main.a0700193.chunk.js"
+  },
+  {
+    "revision": "ac6179d0c54513e9cfa2",
+    "url": "/static/js/runtime-main.5288a424.js"
+  }
+]);
